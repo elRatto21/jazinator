88bb89a925bcdcb3cca2110657c6a767aa372881
Niklas Trapp <niklas031712@gmail.com>
2024-05-17 14:24:19 +0200

    🐎 Speed Insights

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-      <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <SpeedInsights />
+  </>
+);
